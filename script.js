be163fc8ff566927725caa281c1a240f962465a8
Mike <foreign.guy.mike@gmail.com>
2025-01-25 15:48:20 -0600

    twilight run

--- script.js
+++ script.js
@@ -14,43 +14,55 @@ const ld30Urls = [
 	["imgs/ld301.png", "imgs/ld301.mp4"]
 ];
 
+var trCurrentIndex = -1;
+const trUrls = [
+	["imgs/tr1.png", "imgs/tr1.png"],
+	["imgs/tr2.png", "imgs/tr2.png"],
+	["imgs/tr3.png", "imgs/tr3.png"]
+];
+
 var aCurrentIndex = -1;
 const aUrls = [
 	["imgs/a1.png", "imgs/a1.mp4"]
 ];
 
-function changeImageFSF(index) {
-	if (index == fsfCurrentIndex) return;
+function changeImage(prefix, index, currentIndex, urls, play = true) {
+	if (index == currentIndex) return;
 	
-	const thumbnails = document.getElementById("fsfThumbnails");
-	if (fsfCurrentIndex >= 0) {
-		thumbnails.children[fsfCurrentIndex].classList.remove("active-thumbnail");
+	const thumbnails = document.getElementById(prefix + "Thumbnails");
+	if (currentIndex >= 0) {
+		thumbnails.children[currentIndex].classList.remove("active-thumbnail");
 	}
 	thumbnails.children[index].classList.add("active-thumbnail");
 	
-	fsfCurrentIndex = index;
-	const url = fsfUrls[index][1];
-	const mainImage = document.getElementById("fsfMainImage");
-	const mainVideo = document.getElementById("fsfMainVideo");
-	const mainVideoSource = document.getElementById("fsfMainVideoSource");
+	const url = urls[index][1];
+	const mainImage = document.getElementById(prefix + "MainImage");
+	const mainVideo = document.getElementById(prefix + "MainVideo");
+	const mainVideoSource = document.getElementById(prefix + "MainVideoSource");
 	if (isImage(url)) {
-		mainVideo.style.display = "none";
-		mainVideo.pause();
+		if (mainVideo != null) {
+			mainVideo.style.display = "none";
+			mainVideo.pause();
+		}
 		mainImage.style.display = "block";
 		mainImage.src = url;
 	} else if (isVideo(url)) {
 		mainImage.style.display = "none";
-		mainVideo.style.display = "block";
-		if (mainVideoSource.src != url) {
-			const ext = getUrlExtension(url);
-			mainVideoSource.setAttribute("src", url);
-			mainVideoSource.setAttribute("type", "video/" + ext);
-			mainVideo.load();
-			mainVideo.addEventListener("canplay", function() {
-				mainVideo.play();
-			});
-		} else {
-			mainVideo.play();
+		if (mainVideo != null) {
+			mainVideo.style.display = "block";
+		}
+		if (mainVideo != null) {
+			if (mainVideoSource.src != url) {
+				const ext = getUrlExtension(url);
+				mainVideoSource.setAttribute("src", url);
+				mainVideoSource.setAttribute("type", "video/" + ext);
+				mainVideo.load();
+				mainVideo.addEventListener("canplay", function() {
+					if (play) mainVideo.play();
+				});
+			} else {
+				if (play) mainVideo.play();
+			}
 		}
 	}
 }
@@ -134,11 +146,24 @@ function populateThumbnails() {
         thumbnail.src = imageUrl[0];
         thumbnail.alt = "Thumbnail ${index + 1}";
         thumbnail.onclick = function () {
-            changeImageFSF(index);
+            changeImage("fsf", index, fsfCurrentIndex, fsfUrls);
         };
         thumbnails.appendChild(thumbnail);
     });
-	changeImageFSF(0);
+	changeImage("fsf", 0, fsfCurrentIndex, fsfUrls);
+	
+	thumbnails = document.getElementById("twilightRunThumbnails");
+	trUrls.forEach((imageUrl, index) => {
+		const thumbnail = document.createElement("img");
+		thumbnail.src = imageUrl[0];
+		thumbnail.alt = "Thumbnail ${index + 1}";
+		thumbnail.onclick = function() {
+			changeImage("twilightRun", index, trCurrentIndex, trUrls);
+			trCurrentIndex = index;
+		};
+		thumbnails.appendChild(thumbnail);
+	});
+	changeImage("twilightRun", 0, trCurrentIndex, trUrls, false);
 	
 	thumbnails = document.getElementById("ld30Thumbnails");
 	ld30Urls.forEach((imageUrl, index) => {
@@ -146,11 +171,11 @@ function populateThumbnails() {
         thumbnail.src = imageUrl[0];
         thumbnail.alt = "Thumbnail ${index + 1}";
         thumbnail.onclick = function () {
-            changeImageLD30(index);
+            changeImageLD30("ld30", index, ld30CurrentIndex, ld30Urls);
         };
         thumbnails.appendChild(thumbnail);
     });
-	changeImageLD30(0, false);
+	changeImage("ld30", 0, ld30CurrentIndex, ld30Urls, false);
 	
 	thumbnails = document.getElementById("aThumbnails");
 	aUrls.forEach((imageUrl, index) => {
@@ -158,11 +183,11 @@ function populateThumbnails() {
         thumbnail.src = imageUrl[0];
         thumbnail.alt = "Thumbnail ${index + 1}";
         thumbnail.onclick = function () {
-            changeImageA(index);
+            changeImage("a", index, aCurrentIndex, aUrls, false);
         };
         thumbnails.appendChild(thumbnail);
     });
-	changeImageA(0, false);
+	changeImage("a", 0, aCurrentIndex, aUrls, false);
 }
 
 function isImage(url) {
